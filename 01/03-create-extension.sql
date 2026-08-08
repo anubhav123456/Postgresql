c9d76334d30e7e0b0955d2e13eb9c0d41d82a0ef
anubhavgit786 <anubhavgupta491@gmail.com>
2026-08-08 10:53:59 +0530

    Part 01

--- 01/03-create-extension.sql
+++ 01/03-create-extension.sql
@@ -1,12 +1,20 @@
+-- =====================================================
+-- PostgreSQL Schema Basics
+-- =====================================================
+
+-- 1. Enable pgcrypto extension
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-SELECT schema_name FROM information_schema.schemata ORDER BY schema_name;
+-- 2. Get all schemas
+SELECT schema_name
+FROM information_schema.schemata
+ORDER BY schema_name;
 
-```text
-schema_name
-----------------
-information_schema
-pg_catalog
-pg_toast
-public
-```
+-- Expected Output:
+--
+-- schema_name
+-- ----------------
+-- information_schema
+-- pg_catalog
+-- pg_toast
+-- public
